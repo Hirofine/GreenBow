ae51692ddd18eaa26830e0dca55d42edc204796f
Josue Becerra <josuebecerra.7@gmail.com>
2020-11-05 18:11:35 +0100

    comments

--- Website/script.js
+++ Website/script.js
@@ -1,14 +1,8 @@
-
 function demoFromHTML() {
   var pdf = new jsPDF('p', 'pt', 'letter');
   // source can be HTML-formatted string, or a reference
   // to an actual DOM element from which the text will be scraped.
   source = $('#customers')[0];
-
-  // we support special element handlers. Register them with jQuery-style 
-  // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-  // There is no support for any other type of selectors 
-  // (class, of compound) at this time.
   specialElementHandlers = {
     // element with id of "bypass" - jQuery style selector
     '#bypassme': function(element, renderer) {
@@ -21,22 +15,17 @@ function demoFromHTML() {
     bottom: 60,
     left: 40,
     width: 2000,
-  
   };
-  // all coords and widths are in jsPDF instance's declared units
-  // 'inches' in this case
+  // Get HTML fromat into PDF
   pdf.fromHTML(
     source, // HTML string or DOM elem ref.
     margins.left, // x coord
     margins.top, { // y coord
       'width': margins.width, // max width of content on PDF
       'elementHandlers': specialElementHandlers,
-    
     },
-
+    // function to download pdf
     function(dispose) {
-      // dispose: object with X, Y of the last line add to the PDF 
-      //          this allow the insertion of new lines after html
       pdf.save('Results.pdf');
     }, margins);
 }
