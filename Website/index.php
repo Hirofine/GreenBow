a5aa45f637dcd46bccfe230318079e0a78c77ca8
Josue Becerra <josuebecerra.7@gmail.com>
2020-11-05 22:59:28 +0100

    keywords

--- Website/index.php
+++ Website/index.php
@@ -1,8 +1,9 @@
 <!-- Desgin4Green Hackaton, Index of Fragility Website Tool -->
-<html>
+<html lang="en">
 <head>
 	<!-- Layout of the website, the colors are specified in the styles.css file -->
 	<meta charset="utf-8">
+	<meta name="keywords" content="Index, Digital, Fragility, Numerical, Competences, Access, Information, Administrative">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Index of Fragility</title>
